8b83e1ab9ad6865811e4da721c0e32a2862ccca6
Brendan Moriarty <brmoriar@microsoft.com>
2018-05-09 15:55:16 -0700

    Added docker daemon configuration to the docker artifact

--- Artifacts/linux-install-docker/linux_install_docker_v1.sh
+++ Artifacts/linux-install-docker/linux_install_docker_v1.sh
@@ -7,3 +7,6 @@ installationStatus=$(echo $?)
 if [ $installationStatus -eq 127 ] ; then
     wget -qO- https://get.docker.com/ | sh
 fi
+
+[ -n "$1" ] && echo $1 > /etc/docker/daemon.json
+sudo systemctl restart docker
