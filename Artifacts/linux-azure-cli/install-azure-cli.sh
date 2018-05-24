b634923fbd8ed0b684b055d80636d2af71a296b4
Brendan Moriarty <brmoriar@microsoft.com>
2018-05-24 15:18:22 -0700

    Adding Azure CLI artifact

---
+++ Artifacts/linux-azure-cli/install-azure-cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+     tee /etc/apt/sources.list.d/azure-cli.list
+
+apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+apt-get install apt-transport-https
+apt-get update && apt-get install azure-cli
